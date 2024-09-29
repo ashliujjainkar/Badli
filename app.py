fd75d8649cab60834b8952fdc918a24f9adc38a5
Ashli Ujjainkar <ashliujjainkar@gmail.com>
2024-09-29 16:14:00 +0530

    update version and fix app.py

--- app.py
+++ app.py
@@ -1,58 +1,55 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from bson import ObjectId
 import os
 
 app = Flask(__name__)
 
-# with open('tasks.json','r') as f:
-#     tasks = json.load(f)
-
-version_file_path = os.path.join(os.getcwd(),"version.txt")
+version_file_path = os.path.join(os.getcwd(), "version.txt")
 api_version = "0.0"
 if os.path.exists(version_file_path):
-    with open(version_file_path,"r") as f:
+    with open(version_file_path, "r") as f:
         api_version = f.read()
-print("API Version:",api_version)
+print("API Version:", api_version)
 
-mongo_uri = os.environ.get('MONGO_URI','mongodb://localhost:27017')
+mongo_uri = os.environ.get('MONGO_URI', 'mongodb://localhost:27017')
 client = MongoClient(mongo_uri)
 db = client['tododb']
 tasks_collection = db['tasks']
 
 def task_serializer(task):
     return {
-        '_id': str(task['_id']),
-        'task': task
+        'id': str(task['id']),  
+        'task': task['task'],
+        'completed': task['completed']
     }
-
 
 @app.route('/version', methods=['GET'])
 def get_version():
-    version = {"version":api_version}
-    return jsonify(version)
+    return jsonify({"version": api_version})
 
 @app.route('/list', methods=['GET'])
 def list_tasks():
-    tasks = list(tasks_collection.find({},{'_id': 0}))
+    tasks = list(tasks_collection.find({}, {'_id': 0}))
     return jsonify(tasks)
 
 @app.route('/add', methods=['POST'])
 def add_task():
     new_task = request.json.get('task')
+    print("New task:",new_task, flush=True)
     if new_task:
         task_id = tasks_collection.count_documents({})+1
-        task = {'id':task_id, 'task': new_task, 'completed': False}
-        tasks_collection.insert_one(task)
-        print(task)
+        task = {'id': task_id,'task': new_task, 'completed': False} 
+        print("Task ID:",task['id'], flush=True) 
         return jsonify(task_serializer(task)), 201
     return jsonify({"error": "No task provided"}), 400
 
 @app.route('/complete', methods=['POST'])
 def complete_task():
     task_id = request.json.get('id')
-    task = tasks_collection.find_one({"id":task_id},{'_id':0})
+    task = tasks_collection.find_one({"id": task_id})  
     if task:
-        tasks_collection.update_one({"id":task_id},{"$set": {"completed":True}})
+        tasks_collection.update_one({"id": task_id}, {"$set": {"completed": True}})
         task['completed'] = True
         return jsonify(task_serializer(task))
     return jsonify({"error": "Task not found"}), 404
@@ -60,19 +57,17 @@ def complete_task():
 @app.route('/incomplete', methods=['POST'])
 def incomplete_task():
     task_id = request.json.get('id')
-    task = tasks_collection.find_one({"id":task_id},{'_id':0})
+    task = tasks_collection.find_one({"id": task_id})  
     if task:
-        tasks_collection.update_one({"id": task_id},{"$set":{"completed":False}})
+        tasks_collection.update_one({"id": task_id}, {"$set": {"completed": False}})
         task['completed'] = False
         return jsonify(task_serializer(task))
-    return jsonify({"error":"Task not found"}), 404
+    return jsonify({"error": "Task not found"}), 404
 
-@app.route('/dump' , methods=['GET'])
+@app.route('/dump', methods=['GET'])
 def dump():
-    tasks = list(tasks_collection.find({},{'_id': 0}))
+    tasks = list(tasks_collection.find({}, {'_id': 0}))
     return jsonify({"message": f"{len(tasks)} tasks stored in MongoDB"})
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
-    
